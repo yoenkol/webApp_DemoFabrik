24db9b63ba33200ed0866b0d834ce0c3a0af6ee9
Julius Mateo <julius-mateo@web.de>
2018-11-07 12:43:39 +0100

    quiz.js Aufbau Fragen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 var turn=0;
 
+
+$(function() {
+  //Hauptfunktion
+});
+
+$(".start").click(function() {
+  console.log( "Start" );
+  $(".quiz_start").fadeOut(function() {
+    startQuiz();  
+});
+   
+});
+
 function startQuiz() {
   points = 0;
   currentQuestionNo = 0;
@@ -8,6 +21,48 @@ function startQuiz() {
 
   $("#continue_btn").hide();
 }
+
+$("#answer_a_btn").click(function() {
+  selectAnswer("#answer_a_btn");
+  deselectAnswer("#answer_b_btn");
+  deselectAnswer("#answer_c_btn");
+  deselectAnswer("#answer_d_btn");
+});
+
+$("#answer_b_btn").click(function() {
+  selectAnswer("#answer_b_btn");
+  deselectAnswer("#answer_a_btn");
+  deselectAnswer("#answer_c_btn");
+  deselectAnswer("#answer_d_btn");
+});
+
+$("#answer_c_btn").click(function() {
+  selectAnswer("#answer_c_btn");
+  deselectAnswer("#answer_b_btn");
+  deselectAnswer("#answer_a_btn");
+  deselectAnswer("#answer_d_btn");
+});
+
+$("#answer_d_btn").click(function() {
+  selectAnswer("#answer_d_btn");
+  deselectAnswer("#answer_b_btn");
+  deselectAnswer("#answer_c_btn");
+  deselectAnswer("#answer_a_btn");
+});
+
+$("#answer_commit_btn").click(function() {
+  validateAnswer();
+});
+
+$(".restart").click(function() {
+  $(".quiz_end").fadeOut(function() { 
+    startQuiz();
+    document.getElementById("answer_commit_btn").removeAttribute("display");
+  });
+
+   
+});
+
 
 $("#continue_btn").click(function() {
 if(currentQuestionNo==9){
